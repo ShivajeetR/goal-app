ada9b0d545783329ddc47a44805bb5c8e1d02660
ShivajeetR <rai.shivajeet@gmail.com>
2022-08-15 12:33:47 +0100

    TS update

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
-import React from "react";
+import {StyleSheet, Text, View, Pressable} from 'react-native';
+import React from 'react';
 
 function GoalItem(props) {
   return (
     <View style={styles.goalItem}>
-      <Pressable  style={({pressed}) => pressed && styles.pressedItem} onPress={props.onDeleteItem.bind(this, props.id)}>
+      <Pressable
+        style={({pressed}) => pressed && styles.pressedItem}
+        onPress={props.onDeleteItem.bind(this, props.id)}>
         <Text style={styles.goalText}>{props.text}</Text>
       </Pressable>
     </View>
@@ -17,13 +19,13 @@ const styles = StyleSheet.create({
   goalItem: {
     margin: 10,
     borderRadius: 5,
-    padding : 10,
-    backgroundColor: "#5e0acc",
+    padding: 10,
+    backgroundColor: '#5e0acc',
   },
   pressedItem: {
-    opacity: 0.5
+    opacity: 0.5,
   },
   goalText: {
-    color: "white",
-  }
+    color: 'white',
+  },
 });
